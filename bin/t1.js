91f824f7c3d6d956066ca807613940d0b8573e5b
Alex Sun <xsun@verisk.com>
2017-08-11 17:10:39 -0400

    add more stuff

--- bin/t1.js
+++ bin/t1.js
@@ -14,5 +14,10 @@
     var f1;
     f1 = f;
     f1([2, 3]);
+    // ...
+    var _a = [1, 2, 3, 4], a1 = _a[0], a2 = _a[1], aRest = _a.slice(2);
+    console.log(a1);
+    console.log(a2);
+    console.log(aRest);
 })();
 //# sourceMappingURL=c:/Code/TypeScript_New/bin/t1.js.map
--- t1.ts
+++ t1.ts
@@ -12,8 +12,34 @@
     }
     f([1, 2]);
 
-    let f1 : ([a, b]) => void;
+    let f1: ([a, b]) => void;
     f1 = f;
 
-    f1([2,3]);
+    f1([2, 3]);
+
+    // ...
+    let [a1, a2, ...aRest] = [1, 2, 3, 4];
+    console.log(a1);
+    console.log(a2);
+    console.log(aRest);
+
+    // object destructing
+    let b1 = {
+        "name": "Alex",
+        "age": 28,
+        "geneder": "M"
+    };
+
+    let { name: n1, age: n2 } = b1;
+    let { name: m1, ...m2 } = b1;
+    console.log(m1);
+    console.log(m2.age);
+    console.log(m2.geneder);
+
+    // default value
+    let f2: (a: string, b?: number) => any;
+    function keepWholeObject(wholeObject: { a: string, b?: number }) {
+        let { a, b = 1001 } = wholeObject;
+    }
+
 })();
